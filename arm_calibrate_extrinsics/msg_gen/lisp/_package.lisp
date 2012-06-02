21154272ec6836c5915bae9ece4aa5423821a7c7
Arm Test User <arm@duerer.usc.edu>
2012-06-01 18:26:37 -0700

    Ignoring generated files

--- arm_calibrate_extrinsics/msg_gen/lisp/_package.lisp
+++
@@ -1,9 +0,0 @@
-(cl:defpackage arm_calibrate_extrinsics-msg
-  (:use )
-  (:export
-   "<ARFRAME>"
-   "ARFRAME"
-   "<ARFRAME3D>"
-   "ARFRAME3D"
-  ))
-
--- arm_head_control/msg_gen/lisp/_package_LookAtActionResult.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package arm_head_control-msg)
-(cl:export '(HEADER-VAL
-          HEADER
-          STATUS-VAL
-          STATUS
-          RESULT-VAL
-          RESULT
-))
